f6789bbf67511a8ecaee6eefa3a2a9c5f6cfe708
cgtbatista <c203748@dac.unicamp.br>
2024-08-28 17:15:48 -0300

    "Added new test set 'density.jl -- TRUE POSITIVES' and removed old test set 'LumberjackToolkit.jl'; added a test case to check vector lengths in densityprofile function."

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using LumberjackToolkit
 using Test
 
-@testset "LumberjackToolkit.jl" begin
+### MAIN TESTS
+@testset "density.jl -- TRUE POSITIVES" begin
+    # Test 1: Matching the vector lengths
+    dist, dens = densityprofile("popc/popc.pdb", "popc/popc.dcd", profile="number", selection="not water")
+    @test length(dist) == length(dens)
     # Write your tests here.
 end
