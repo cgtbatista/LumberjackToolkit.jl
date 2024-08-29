0e9afd937cf2c969ca2b122af7582f460537d197
cgtbatista <c203748@dac.unicamp.br>
2024-08-29 09:23:53 -0300

    "Updated runtests.jl to import Testing module and replaced file paths with variables in densityprofile function call."

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using LumberjackToolkit
-using Test
+using Test, Testing
 
 ### MAIN TESTS
 @testset "density.jl -- TRUE POSITIVES" begin
     # Test 1: Matching the vector lengths
-    dist, dens = densityprofile("popc/popc.pdb", "popc/popc.dcd", profile="number", selection="not water")
+    dist, dens = densityprofile(popc_pdb, popc_pdb, profile="number", selection="not water")
     @test length(dist) == length(dens)
     # Write your tests here.
 end
