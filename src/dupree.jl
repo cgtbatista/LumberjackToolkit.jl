1e0893ae132c719ee17c7912f278bfb6d9a14b88
Carlos G T Batista <c203748@dac.unicamp.br>
2025-02-12 21:31:13 -0300

    Add VSCode settings for Julia environment path

--- src/dupree.jl
+++ src/dupree.jl
@@ -134,84 +134,6 @@ function filterSTL(stlname::String)
     return unique(STL)
 end
 
-function teste2(;data="/home/carlos/Documents/Sandbox/paul/cellulose.dat", nbins=100)
-    xyz = readdlm(data)
-    x, y, z = xyz[:,1], xyz[:,2], xyz[:,3]
-    zmin, zmax = extrema(z)
-    bin_edges = range(
-        zmin,
-        zmax,
-        length=nbins+1
-    )
-    diameters = Float64[]
-    for i in 1:nbins
-        lower = bin_edges[i]
-        upper = i < nbins ? bin_edges[i+1] : zmax + eps()
-        mask = (z .>= lower) .& (z .< upper)
-        slice_x = x[mask]
-        slice_y = y[mask]
-        length(slice_x) < 2 && continue ## skip if there are less than 2 points
-        d = 0.0
-        n_pontos = length(slice_x)
-        for j in 1:n_pontos
-            for k in (j+1):n_pontos
-                dist = hypot(slice_x[j] - slice_x[k], slice_y[j] - slice_y[k])
-                d = max(d, dist)
-            end
-        end
-        
-        push!(diameters, d)
-    end
-    
-    # Gerar histograma
-    Plots.histogram(diameters, bins=20, xlabel="Diâmetro", ylabel="Frequência",
-             title="Distribuição de Diâmetros", legend=false)
-    
-    return (d_mean = mean(diameters),
-            d_media = median(diameters),
-            d_sd = std(diameters),
-            hist = Plots.current())
-end
-
-function diameter_analysis(;fibrildata="/home/carlos/Documents/Sandbox/paul/m23432_data.dat")
-    data = readdlm(fibrildata, ',')
-    Θ, diameter, dresidues = [], [], convert(Vector{Int64}, data[:,1])
-    dΘ = missing
-    for i in unique(dresidues)
-        idx = findall(x -> x == i, dresidues)
-        radii_info = data[idx,2]
-        angle_info = data[idx,3]
-        for idx_angle in eachindex(angle_info)
-            if angle_info[idx_angle] < 0.0
-                angle_info[idx_angle] = angle_info[idx_angle] + 360.0
-            end
-        end
-        dΘ = std(diff(angle_info))
-        for j in eachindex(idx)
-            tmp_radius = radii_info[j]; tmp_angle = angle_info[j];
-            if tmp_angle < 180.0
-                reciprocal_angle = tmp_angle + 180.0
-            else
-                reciprocal_angle = tmp_angle - 180.0
-            end
-            lower_bound = reciprocal_angle-dΘ
-            upper_bound = reciprocal_angle+dΘ
-            nearest_angles = findall(x ->
-                (x >= lower_bound) && (x <= upper_bound), angle_info)
-            median_idx = Int64(round(median(nearest_angles)))
-            tmp_radius2 = radii_info[median_idx]
-            push!(Θ, round(tmp_angle, digits=3))
-            push!(diameter, round(tmp_radius+tmp_radius2, digits=3))
-        end
-    end
-
-    Θ = convert(Vector{Float64}, Θ)
-    diameter = convert(Vector{Float64}, diameter)
-
-    return dresidues, diameter, Θ
-
-end
-
 function chain_centers(pdbname::String; Δz::Float64=0.1, cutoff::Float64=0.5, isreference::Bool=true)
     center = Vector{SVector{3, Float64}}()
     atoms = PDBTools.readPDB(pdbname)
@@ -260,7 +182,7 @@ function fibrilwidth(
 end
 
 function scan_diameter(xyz::Vector{SVector{3, Float64}}, centers::Vector{SVector{3, Float64}}; step=1.0, tol=0.05)
-    d, azimuth, inclination = Float64[], Float64[], Float64[]
+    d, Φ, Θ = Float64[], Float64[], Float64[]
     for center in centers
         isurface = findall(k -> norm(k - center[3]) <= tol, [ ijk[3] for ijk in xyz ])
         surface = [ surf - center for surf in xyz[isurface] ]
@@ -275,55 +197,11 @@ function scan_diameter(xyz::Vector{SVector{3, Float64}}, centers::Vector{SVector
             acosd.(z ./ r),
             360.0
         )
-        a, b, c = getradii(r, ϕ, θ, step=step, tol=tol)
-        append!(d, a); append!(azimuth, b); append!(inclination, c)
+        r, azimuth, inclination = getradii(r, ϕ, θ, step=step, tol=tol)
+        append!(d, r); append!(Φ, azimuth); append!(Θ, inclination)
     end
-    return d, azimuth, inclination
+    return d, Φ, Θ  ## I will remove the inlcination later, cause I only need the azimuthal angle to calculate the diameter
 end
-
-# function scan_diameter(xyz::Vector{SVector{3, Float64}}, centers::Vector{SVector{3, Float64}}; Δθ=1.0, tol=0.05)
-#     d = Float64[]
-#     for center in centers
-#         isurface = findall(k -> norm(k - center[3]) <= tol, [ ijk[3] for ijk in xyz ])
-#         surface = [ surf - center for surf in xyz[isurface] ]
-#         r = norm.(surface)
-#         x, y, z = [ s[1] for s in surface ], [ s[2] for s in surface ], [ s[3] for s in surface ]
-#         norm_xy = norm.([ s[1:2] for s in surface ])
-#         θ = sign.(y) .* acosd.(x ./ norm_xy)
-#         ϕ = acosd.(z ./ r)
-#         θ, ϕ = mod.(θ, 360.), mod.(ϕ, 360.)
-#         append!(d, getwidth(r, θ, ϕ, step=Δθ, tol=tol))
-#     end
-#     return d
-# end
-
-# function getwidth(r::Vector{Float64}, θ::Vector{Float64}, ϕ::Vector{Float64}; Δθ=1.0, tol=0.05)
-#     radii = Float64[]
-#     for angle in 0.0:Δθ:360.0
-#         θi, θf = mod(angle - 0.5*Δθ, 360.0), mod(angle + 0.5*Δθ, 360.0)
-#         idxθ = θi < θf ? findall(i -> (i >= θi) && (i <= θf), θ) : findall(i -> (i >= θi) || (i <= θf), θ)
-#         rtemp, theta, phi = Vector{Float64}(undef, length(idxθ)), Vector{Float64}(undef, length(idxθ)), Vector{Float64}(undef, length(idxθ))
-#         token = 1
-#         for idx in idxθ
-#             ϕ1, ϕ2 = mod(ϕ[idx] - tol, 360.0), mod(ϕ[idx] + tol, 360.0)
-#             idxϕ = ϕ1 < ϕ2 ? findall(i -> (i >= ϕ1) && (i <= ϕ2), ϕ) : findall(i -> (i >= ϕ1) || (i <= ϕ2), ϕ)
-#             if isempty(idxϕ)
-#                 continue
-#             end
-#             iradii = argmax(r[idxϕ])
-#             rtemp[token] = r[iradii]
-#             println("Angle: $(θ[idx]) - Radius: $(r[iradii]) - Φ: $(ϕ[iradii])")
-#             token += 1
-#         end
-#         if isempty(rtemp)
-#             continue
-#         else
-#             push!(radii, median(rtemp))
-#         end
-#         println("")
-#     end
-#     return 2 * radii
-# end
 
 function getradii(
     radii::Vector{Float64},
@@ -357,83 +235,172 @@ function getradii(
     return ρ, ϕ, θ
 end
 
-# dΘ = std(diff(angle_info))
-# for j in eachindex(idx)
-#     tmp_radius = radii_info[j]; tmp_angle = angle_info[j];
-#     if tmp_angle < 180.0
-#         reciprocal_angle = tmp_angle + 180.0
-#     else
-#         reciprocal_angle = tmp_angle - 180.0
-#     end
-#     lower_bound = reciprocal_angle-dΘ
-#     upper_bound = reciprocal_angle+dΘ
-#     nearest_angles = findall(x ->
-#         (x >= lower_bound) && (x <= upper_bound), angle_info)
-#     median_idx = Int64(round(median(nearest_angles)))
-#     tmp_radius2 = radii_info[median_idx]
-#     push!(Θ, round(tmp_angle, digits=3))
-#     push!(diameter, round(tmp_radius+tmp_radius2, digits=3))
-# end
+function edp(
+    psfname::String,
+    pdbname::String;
+    )
 
-function fibrilradii(
-    pdb::String,
-    file::String;
-    Δz=0.01,
-    steps=false, Δstep=0.1
-)
-    if (steps == true) && (Δstep == 0.0)
-        throw(ArgumentError("The Δstep must be greater than zero."))
-    end
-    if Δstep <= Δz
-        throw(ArgumentError("The Δstep ($Δstep Å) should be greater than Δz ($Δz Å) to avoid duplicate radius quantification."))
-    end
-    atoms = PDBTools.readPDB(pdb)
-    lower_cutoff = mean([
-            atom.z for atom in PDBTools.select(atoms, atom -> atom.resnum == minimum(PDBTools.resnum.(atoms)))
-        ])
-    upper_cutoff = mean([
-            atom.z for atom in PDBTools.select(atoms, atom -> atom.resnum == maximum(PDBTools.resnum.(atoms)))
-        ])
-    data = readdlm(file)
-    xyz = unique(
-            [ [irow[1], irow[2], irow[3]] for irow in eachrow(data) ]
-        )
-    zaxis = [ ijk[3] for ijk in xyz ]
-    xyz, zaxis, ztoken = xyz[sortperm(zaxis)], zaxis[sortperm(zaxis)], 0.
-    r = Float64[]; ϕ = Float64[]; θ = Float64[]; central_vectors = Vector{Float64}[]; labels = Int64[];
-    for (i, z) in enumerate(zaxis)
-        if (z < lower_cutoff) || (z > upper_cutoff)
-            continue
-        end
-        if any(test -> test == true, steps)
-            if (i != 1)
-                if abs(z-ztoken) < Δstep
-                    continue
-                else
-                    ztoken = z
-                end
-            else
-                ztoken = z
-            end
-        end
-        Δatoms = PDBTools.select(atoms, atom -> (atom.z >= z - Δz) && (atom.z <= z + Δz))
-        xyz0 = [ mean([ xyz[1] for xyz in PDBTools.coor(Δatoms) ]), mean([ xyz[2] for xyz in PDBTools.coor(Δatoms) ]), mean([ xyz[3] for xyz in PDBTools.coor(Δatoms) ]) ]
-        if isnan(mean(xyz0)); continue; end
-        push!(central_vectors, xyz0)
-        for idx in findall(z -> (z >= z - Δz) && (z <= z + Δz), zaxis)
-            ## picking {r, θ, ϕ} -- following the physical spherical coordinates classification as { radius, azimuthal, polar }
-            vector_xyz = xyz[idx] - xyz0
-            idx_r = norm(vector_xyz)
-            idx_θ = sign(vector_xyz[2])*acosd(vector_xyz[1]/norm(vector_xyz[1:2]))
-            idx_ϕ = acosd(vector_xyz[3]/norm(vector_xyz))
-            if idx_θ < 0; idx_θ += 360; end
-            if idx_ϕ < 0; idx_ϕ += 360; end
-            ## pushing the data
-            push!(r, idx_r); push!(θ, idx_θ); push!(ϕ, idx_ϕ); push!(labels, i)
-        end
-    end
-    return xyz, central_vectors, labels, [ r, θ, ϕ ]
-end
+# function fibrilradii(
+#     pdb::String,
+#     file::String;
+#     Δz=0.01,
+#     steps=false, Δstep=0.1
+# )
+#     if (steps == true) && (Δstep == 0.0)
+#         throw(ArgumentError("The Δstep must be greater than zero."))
+#     end
+#     if Δstep <= Δz
+#         throw(ArgumentError("The Δstep ($Δstep Å) should be greater than Δz ($Δz Å) to avoid duplicate radius quantification."))
+#     end
+#     atoms = PDBTools.readPDB(pdb)
+#     lower_cutoff = mean([
+#             atom.z for atom in PDBTools.select(atoms, atom -> atom.resnum == minimum(PDBTools.resnum.(atoms)))
+#         ])
+#     upper_cutoff = mean([
+#             atom.z for atom in PDBTools.select(atoms, atom -> atom.resnum == maximum(PDBTools.resnum.(atoms)))
+#         ])
+#     data = readdlm(file)
+#     xyz = unique(
+#             [ [irow[1], irow[2], irow[3]] for irow in eachrow(data) ]
+#         )
+#     zaxis = [ ijk[3] for ijk in xyz ]
+#     xyz, zaxis, ztoken = xyz[sortperm(zaxis)], zaxis[sortperm(zaxis)], 0.
+#     r = Float64[]; ϕ = Float64[]; θ = Float64[]; central_vectors = Vector{Float64}[]; labels = Int64[];
+#     for (i, z) in enumerate(zaxis)
+#         if (z < lower_cutoff) || (z > upper_cutoff)
+#             continue
+#         end
+#         if any(test -> test == true, steps)
+#             if (i != 1)
+#                 if abs(z-ztoken) < Δstep
+#                     continue
+#                 else
+#                     ztoken = z
+#                 end
+#             else
+#                 ztoken = z
+#             end
+#         end
+#         Δatoms = PDBTools.select(atoms, atom -> (atom.z >= z - Δz) && (atom.z <= z + Δz))
+#         xyz0 = [ mean([ xyz[1] for xyz in PDBTools.coor(Δatoms) ]), mean([ xyz[2] for xyz in PDBTools.coor(Δatoms) ]), mean([ xyz[3] for xyz in PDBTools.coor(Δatoms) ]) ]
+#         if isnan(mean(xyz0)); continue; end
+#         push!(central_vectors, xyz0)
+#         for idx in findall(z -> (z >= z - Δz) && (z <= z + Δz), zaxis)
+#             ## picking {r, θ, ϕ} -- following the physical spherical coordinates classification as { radius, azimuthal, polar }
+#             vector_xyz = xyz[idx] - xyz0
+#             idx_r = norm(vector_xyz)
+#             idx_θ = sign(vector_xyz[2])*acosd(vector_xyz[1]/norm(vector_xyz[1:2]))
+#             idx_ϕ = acosd(vector_xyz[3]/norm(vector_xyz))
+#             if idx_θ < 0; idx_θ += 360; end
+#             if idx_ϕ < 0; idx_ϕ += 360; end
+#             ## pushing the data
+#             push!(r, idx_r); push!(θ, idx_θ); push!(ϕ, idx_ϕ); push!(labels, i)
+#         end
+#     end
+#     return xyz, central_vectors, labels, [ r, θ, ϕ ]
+# end
+# 
+# function get_diameters(radiidata::Vector{Vector{Float64}}; threshold=1.5, tolerance=0.05)
+#     ## The tolerance is related to the angle θ and the marginal error to be outsisde the plane of the fibril is related to the angle ϕ
+#     dθ = threshold; dϕ = tolerance;
+#     diameters = Float64[]
+#     for ith in eachindex(radiidata[1])
+#         r = radiidata[1][ith]; θ = radiidata[2][ith]; ϕ = radiidata[3][ith];
+#         new_θ = θ + 180.; lower_bound = mod(new_θ - dθ, 360.); upper_bound = mod(new_θ + dθ, 360.);
+#         if lower_bound < upper_bound
+#             raw_idx = findall(θi -> θi >= lower_bound && θi <= upper_bound, radiidata[2])
+#         else
+#             raw_idx = findall(θi -> θi >= lower_bound || θi <= upper_bound, radiidata[2])
+#         end
+#         idx = findall(ϕi -> ϕi >= mod(ϕ - dϕ, 360) && ϕi <= mod(ϕ + dϕ, 360), radiidata[3][raw_idx])
+#         if length(idx) == 0
+#             continue
+#         else
+#             inv_r = radiidata[1][idx]; inv_θ = radiidata[2][idx]; inv_ϕ = radiidata[3][idx]
+#             distances = @. sqrt((r)^2 + (inv_r)^2 - 2*(r)*(inv_r)*(sind(inv_ϕ)*sind(ϕ)*cosd(inv_θ-θ)+cosd(inv_ϕ)*cosd(ϕ)))
+#             push!(diameters, median(distances))
+#         end
+#     end
+#     return diameters
+# end
+# 
+# function teste2(;data="/home/carlos/Documents/Sandbox/paul/cellulose.dat", nbins=100)
+#     xyz = readdlm(data)
+#     x, y, z = xyz[:,1], xyz[:,2], xyz[:,3]
+#     zmin, zmax = extrema(z)
+#     bin_edges = range(
+#         zmin,
+#         zmax,
+#         length=nbins+1
+#     )
+#     diameters = Float64[]
+#     for i in 1:nbins
+#         lower = bin_edges[i]
+#         upper = i < nbins ? bin_edges[i+1] : zmax + eps()
+#         mask = (z .>= lower) .& (z .< upper)
+#         slice_x = x[mask]
+#         slice_y = y[mask]
+#         length(slice_x) < 2 && continue ## skip if there are less than 2 points
+#         d = 0.0
+#         n_pontos = length(slice_x)
+#         for j in 1:n_pontos
+#             for k in (j+1):n_pontos
+#                 dist = hypot(slice_x[j] - slice_x[k], slice_y[j] - slice_y[k])
+#                 d = max(d, dist)
+#             end
+#         end
+#         
+#         push!(diameters, d)
+#     end
+#     
+#     # Gerar histograma
+#     Plots.histogram(diameters, bins=20, xlabel="Diâmetro", ylabel="Frequência",
+#              title="Distribuição de Diâmetros", legend=false)
+#     
+#     return (d_mean = mean(diameters),
+#             d_media = median(diameters),
+#             d_sd = std(diameters),
+#             hist = Plots.current())
+# end
+# 
+# function diameter_analysis(;fibrildata="/home/carlos/Documents/Sandbox/paul/m23432_data.dat")
+#     data = readdlm(fibrildata, ',')
+#     Θ, diameter, dresidues = [], [], convert(Vector{Int64}, data[:,1])
+#     dΘ = missing
+#     for i in unique(dresidues)
+#         idx = findall(x -> x == i, dresidues)
+#         radii_info = data[idx,2]
+#         angle_info = data[idx,3]
+#         for idx_angle in eachindex(angle_info)
+#             if angle_info[idx_angle] < 0.0
+#                 angle_info[idx_angle] = angle_info[idx_angle] + 360.0
+#             end
+#         end
+#         dΘ = std(diff(angle_info))
+#         for j in eachindex(idx)
+#             tmp_radius = radii_info[j]; tmp_angle = angle_info[j];
+#             if tmp_angle < 180.0
+#                 reciprocal_angle = tmp_angle + 180.0
+#             else
+#                 reciprocal_angle = tmp_angle - 180.0
+#             end
+#             lower_bound = reciprocal_angle-dΘ
+#             upper_bound = reciprocal_angle+dΘ
+#             nearest_angles = findall(x ->
+#                 (x >= lower_bound) && (x <= upper_bound), angle_info)
+#             median_idx = Int64(round(median(nearest_angles)))
+#             tmp_radius2 = radii_info[median_idx]
+#             push!(Θ, round(tmp_angle, digits=3))
+#             push!(diameter, round(tmp_radius+tmp_radius2, digits=3))
+#         end
+#     end
+# 
+#     Θ = convert(Vector{Float64}, Θ)
+#     diameter = convert(Vector{Float64}, diameter)
+# 
+#     return dresidues, diameter, Θ
+# 
+# end
 
 # if centermethods == "real"
 #     selstructure = select(structure, by = atoms -> (atoms.z >= lower_bound) && (atoms.z <= upper_bound))
@@ -455,28 +422,3 @@ end
 # else
 #     throw(ArgumentError("The method $method is not implemented."))
 # end
-
-
-function get_diameters(radiidata::Vector{Vector{Float64}}; threshold=1.5, tolerance=0.05)
-    ## The tolerance is related to the angle θ and the marginal error to be outsisde the plane of the fibril is related to the angle ϕ
-    dθ = threshold; dϕ = tolerance;
-    diameters = Float64[]
-    for ith in eachindex(radiidata[1])
-        r = radiidata[1][ith]; θ = radiidata[2][ith]; ϕ = radiidata[3][ith];
-        new_θ = θ + 180.; lower_bound = mod(new_θ - dθ, 360.); upper_bound = mod(new_θ + dθ, 360.);
-        if lower_bound < upper_bound
-            raw_idx = findall(θi -> θi >= lower_bound && θi <= upper_bound, radiidata[2])
-        else
-            raw_idx = findall(θi -> θi >= lower_bound || θi <= upper_bound, radiidata[2])
-        end
-        idx = findall(ϕi -> ϕi >= mod(ϕ - dϕ, 360) && ϕi <= mod(ϕ + dϕ, 360), radiidata[3][raw_idx])
-        if length(idx) == 0
-            continue
-        else
-            inv_r = radiidata[1][idx]; inv_θ = radiidata[2][idx]; inv_ϕ = radiidata[3][idx]
-            distances = @. sqrt((r)^2 + (inv_r)^2 - 2*(r)*(inv_r)*(sind(inv_ϕ)*sind(ϕ)*cosd(inv_θ-θ)+cosd(inv_ϕ)*cosd(ϕ)))
-            push!(diameters, median(distances))
-        end
-    end
-    return diameters
-end
